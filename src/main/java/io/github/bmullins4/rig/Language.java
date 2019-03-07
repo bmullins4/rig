8a230b96973e4279780e5905a1aa690357b27560
Ben Mullins <bmullins4@murraystate.edu>
2019-03-06 20:42:02 -0600

    Java support
    Added Java enum to support Java language

---
+++ src/main/java/io/github/bmullins4/rig/Language.java
@@ -0,0 +1,40 @@
+
+package io.github.bmullins4.rig;
+
+public enum Language {
+
+	JAVA {
+		@Override
+		public String[] keywords() {
+			
+			String[] kw = {
+				"abstract", "assert", "boolean", "break", "byte", "case",
+				"catch", "char", "class", "const", "continue", "default",
+				"double", "do", "else", "enum", "extends", "false",
+				"final", "finally", "float", "for", "goto", "if",
+				"implements", "import", "instanceof", "int", "interface",
+				"long", "native", "new", "null", "package", "private",
+				"protected", "public", "return", "short", "static", 
+				"strictfp", "super", "switch", "synchronized", "this",
+				"throw", "throws", "transient", "true", "try", "void",
+				"volatile", "while"
+			};
+			
+			return kw;
+		}
+		
+		@Override
+		public String rules() { return "\\b[a-zA-Z_$][a-zA-Z0-9_$]*\\b"; }
+		@Override
+		public String template() {
+			return "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_$";
+		}
+		@Override
+		public String lang() { return "Java"; }
+	};
+	
+	public abstract String[] keywords();
+	public abstract String rules();
+	public abstract String template();
+	public abstract String lang();
+}
--- src/main/java/io/github/bmullins4/rig/RandomIdentifierGenerator.java
+++ src/main/java/io/github/bmullins4/rig/RandomIdentifierGenerator.java
@@ -1,24 +1,39 @@
 
 package io.github.bmullins4.rig;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class RandomIdentifierGenerator {
-
-	private final String language;
 	
-	public RandomIdentifierGenerator(final String language) {
+	private final Language lang;
+	private final int maxLength;
+	
+	private List<String> changedIdentifiers;
+	
+	public RandomIdentifierGenerator(final Language lang, final int maxLength) {
 		
-		this.language = Objects.requireNonNull(language);
+		this.lang = Objects.requireNonNull(lang);
+		if(maxLength < 2)
+			throw new IllegalArgumentException("Length must be > 1");
+		this.maxLength = maxLength;
+		
+		changedIdentifiers = new ArrayList<String>();
 	}
 	
 	public String generate() {
 	
-		return "";
+		String identifier = "";
+		int length = (int) (Math.random() * maxLength + 2);
+		//TODO: implement working part
+		
+		
+		return identifier;
 	}
 	
 	public String getLanguage() {
 		
-		return language;
+		return lang.lang();
 	}
 }
